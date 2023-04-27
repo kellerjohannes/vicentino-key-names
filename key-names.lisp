418b64c7470f710d9b6b4bc13e4205f8a8e24122
Johannes Keller <johannkell@gmail.com>
2023-04-27 17:55:08 +0200

    plist based db handling implemented

--- key-names.lisp
+++ key-names.lisp
@@ -4,10 +4,15 @@
 (in-package :key-names)
 
 
-;;;; approach with plists
+;;;; database handling
 
-(defun make-key (original-name root ordine shorthand)
-  (list :original-name original-name :root root :ordine ordine :shorthand shorthand))
+(defun make-key (libro chapter id original-name root ordine)
+  (list :libro libro
+        :chapter chapter
+        :id id
+        :original-name original-name
+        :root root
+        :ordine ordine))
 
 (defvar *keys* nil)
 
@@ -16,10 +21,48 @@
 (defun dump-keys ()
   (format t "~&~{~{~a:~20t~a~%~}~%~}" *keys*))
 
-(defun save-dbs (filename)
-  (with-open-file (out filename :direction :output :if-exists :supersede)
+(defun prompt-read-string (prompt)
+  (format *query-io* "~&~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-read-integer (prompt)
+  (or (parse-integer (prompt-read-string prompt) :junk-allowed t) 0))
+
+(defun prompt-read-keyword (prompt)
+  (intern (string-upcase (prompt-read-string prompt)) :keyword))
+
+(defun prompt-for-key ()
+  (make-key (prompt-read-integer "Libro [integer 0-5]")
+            (prompt-read-integer "Capitolo [integer > 0]")
+            (prompt-read-integer "Count / ID [integer > 0]")
+            (prompt-read-string "Original name")
+            (prompt-read-keyword "Root key [a-b]")
+            (prompt-read-integer "Ordine [1-6]")))
+
+(defun input-keys ()
+  (loop (add-key (prompt-for-key))
+        (unless (y-or-n-p "Add another key? [y/n]: ") (return))))
+
+(defun save-dbs ()
+  (with-open-file (out "db-keys" :direction :output :if-exists :supersede)
     (with-standard-io-syntax
       (print *keys* out))))
+
+(defun read-dbs ()
+  (with-open-file (in "db-keys")
+    (with-standard-io-syntax
+      (setf *keys* (read in)))))
+
+(defun make-comparison-expression (field value)
+  `(equal (getf entry ,field) ,value))
+
+(defun make-comparison-list (fields)
+  (loop while fields
+        collecting (make-comparison-expression (pop fields) (pop fields))))
+
+(defmacro where (&rest fields)
+  `(lambda (entry) (and ,@(make-comparison-list fields))))
 
 
 
