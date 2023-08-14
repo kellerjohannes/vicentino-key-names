abd5bba624094d7720b49048c7d181b089577abe
Johannes Keller <johannkell@gmail.com>
2023-08-14 23:16:24 +1000

    compile added

---
+++ tex-output/compile-all.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+pdflatex liste-komplett.tex
+pdflatex liste-eingriffe.tex
+pdflatex liste-kritisch.tex
+pdflatex liste-kritische-eingriffe.tex
+pdflatex liste-verkuerzungen.tex
+pdflatex liste-verkuerzungen-vergleich.tex
+
+pdflatex tab-tuning1-alle-intervalle-nach-groesse.tex
+pdflatex tab-tuning1-alle-terzen.tex
+pdflatex tab-tuning1-alle-quinten.tex
+pdflatex tab-tuning1-alle-sexten.tex
+pdflatex tab-tuning1-alle-oktaven.tex
+pdflatex tab-tuning1-alle-semitoni-und-kleiner.tex
+
+pdflatex tab-tuning3-alle-intervalle-nach-groesse.tex
+
+mv *.pdf pdf/
